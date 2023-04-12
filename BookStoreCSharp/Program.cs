cbd78f7ee0814a71f0464d5f23f63a81da5f627a
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-04-12 14:47:46 +0200

    Variables of "Clean Code" book

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿//Book info
+string bookTitle = "Clean Code: A Handbook of Agile Software Craftsmanship";
+string bookAuthor = "Robert C. Martin";
+string bookIsbnCode = "978-0132350884";
+uint pageNumber = 431;
+uint bookWeight = 800;
+double bookWidth = 17.78;
+double bookThickness = 2.79;
+double bookHeigth = 23.37;
+string bookRating = "4,7/5";
+uint bookReviews = 5413;
+bool isBookAvailableOnKindle = true;
+bool isCoverFlexible = true;
+
